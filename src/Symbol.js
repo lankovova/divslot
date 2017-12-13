89ae5d4b1f1ae2ac732b9d4c16e8f98be240c824
lankovova <lanko.vova@gmail.com>
2017-12-13 11:55:54 +0200

    added symbols images names dictionary

--- src/Symbol.js
+++ src/Symbol.js
@@ -3,10 +3,12 @@ import settings from "./settings.json";
 class Symbol {
 	constructor(symbolNumber) {
 		this.symbolNum = symbolNumber;
+
 		this.symbolNode = document.createElement('div');
 		this.symbolNode.style.width = `${settings.symbolSize}px`;
 		this.symbolNode.style.height = `${settings.symbolSize}px`;
-		this.symbolNode.className = `symbol symbol${this.symbolNum}`;
+		this.symbolNode.className = `symbol`;
+		this.symbolNode.style.background = `url('${settings.symbolsPath + settings.symbolsImages[this.symbolNum - 1]}')`;
 	}
 
 	get node() {
