382ddd37d2ade083c356281e5ec1a18601fd58c4
Alexandr Krivosheev <alexandrkrivosheev@Alexandrs-MacBook-Pro.local>
2017-12-14 11:43:46 +0200

    LineController.js test method showWinningLines() created

--- src/LineController.js
+++ src/LineController.js
@@ -1,9 +1,31 @@
 import settings from './settings.json';
+import Line from './Line';
 
 class LineController {
     constructor() {
-
+        this.winningLines = [];
     }
-}
+
+    showWinningLines(reels, gameResult) {
+        let line = new Line('game', 'green');
+        for (const res of gameResult) {
+            const line = new Line('game_wrapper', 'green');
+
+            for (const sCoor of res.list) {
+                // Get reel
+                const reel = this.reels[sCoor.col];
+                // Get winning symbol
+                const symbol = reel.finalSymbols[sCoor.row];
+                // Get symbol coordinates
+                const symbolCoord = symbol.getPosition();
+                console.log(symbolCoord)
+                // Add symbol highlite to line
+                line.addSymbolHighlite(symbolCoord.x, symbolCoord.y);
+            }
+            line.connectHighlites();
+            line.show()
+        }
+    }
+ }
 
 export default LineController;
