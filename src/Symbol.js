95ffdac26f580ec0834189adcf8bbddd699ff35d
Alexandr Krivosheev <alexandrkrivosheev@Alexandrs-MacBook-Pro.local>
2017-12-14 11:42:14 +0200

    added method to get symbol position

--- src/Symbol.js
+++ src/Symbol.js
@@ -14,6 +14,16 @@ class Symbol {
 	get node() {
 		return this.symbolNode;
 	}
+
+	getPosition() {
+		let x = this.symbolNode.getBoundingClientRect();
+		let y = this.symbolNode.getBoundingClientRect();
+		console.log(x.x, y.y)
+		return {
+			'x': x.x,
+			'y': y.y,
+		}
+	}
 }
 
 export default Symbol;
