6276f22261b7535f5cf4a36b6d2a07a34cbf4a08
AlexandrKrivosheev <alexandrkrivosheevmail@gmail.com>
2017-12-17 16:29:13 +0200

    added symbol.highlited property

--- src/Symbol.js
+++ src/Symbol.js
@@ -12,6 +12,7 @@ class Symbol {
 		this.symbolNode.style.width = `${settings.symbolSize}px`;
 		this.symbolNode.style.height = `${settings.symbolSize}px`;
 		this.symbolNode.style.background = `url('${settings.symbolsPath + settings.symbolsImages[this.symbolNum - 1]}')`;
+		this.highlighted = false;
 	}
 
 	get node() {
